49d54f3d389847671e6379df8807e8734577d9f1
believehan <90814456+believehan@users.noreply.github.com>
2025-02-11 11:03:24 +0900

    모듈에서 컴포넌트를 export 할 때 default 지정자를 포함한 경우의 import 형식

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+/* 
+  export 시 default 지정자를 지정함으로써 import 시 객체 참조 지정에 대한
+  블럭{}(객체 구조분해 할당) 생략 가능.
+  ( default 지정자는 모듈 내에서 오직 하나의 함수만 사용가능)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  또한 이경우 export 클래스 명이나 함수명은 import 시의 객체 참조명과 다르거나
+  생략해도 무방. (권장되지는 않음)
+
+  즉, default 지정자 지정 시에는 익명함수 형태의 객체로 내보내기가 가능하며
+  import 시 객체명은 임의로 지정가능.
+  
+  단, 혼란을 방지하기 위해 이경우 default 컴포넌트명과 import 객체 참조명을 
+  동일하게 사용하는 것이 바람직.
+
+  ※ default 컴포넌트는 하나의 모듈에서 오직 하나만 사용가능하지만, default 가
+  아닌 일반 컴포넌트도 해당 모듈에 포함하여 export하는것은 문제되지 않음
+ */
+
+export default function App() {
+  return <h1>모듈 임포트1</h1>;
 }
 
-export default App;
+// export default function Appss() {
+//   return <h1>모듈 임포트1</h1>;
+// }
+
+// export default function () {
+//   return <h1>모듈 임포트1</h1>
+// }
+
+/*
+  default 지정자가 생략 되었으므로, 임포트 시에는 반드시 블럭{}(객체 구조분해 할당)이
+  필요하고 엑스포트 컴포넌트명과 임포트 명이 동일해야함에 주의
+*/
+
+export function App2() {
+  return <h1>모듈 임포트2</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+
 import ReactDOM from 'react-dom/client';
-import TestReact from './TestReact';
-import TestReact2 from './TestReact2';
+
+/*
+  <모듈에서 컴포넌트를 export 할 때 default 지정자를 포함한 경우의 import 형식
+  - 모듈의 객체 참조명에 대한 블럭{}(객체 구조분해 할당) 생략 가능.
+  단, 이는 모듈에서 객체 export 시 default 지정자를 지정한 경우만 가능.
+  또한 이경우 임포트 컴포넌트 객체명(App)을 모듈의 디폴트 함수나 클래스명과
+  다르게 적용하거나 모듈에서의 export 컴포넌트명(함수명) 생략해도 무방. (권장되지는 않음)
+*/
+import App from './App';
+import App2 from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-  {/* 모듈화 된 컴포넌트 호출을 통한 렌더링 요소를 할당 */}
-  <TestReact />
-  <TestReact2 />
+  <App />
+  <App2 />
   </>
-);
+)
