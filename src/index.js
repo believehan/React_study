360c282e3930883c3bef011f1cd1387f25a1718e
believehan <90814456+believehan@users.noreply.github.com>
2025-02-11 09:30:41 +0900

    react 공부시작

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import TestReact from './TestReact';
+import TestReact2 from './TestReact2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <>
+  {/* 모듈화 된 컴포넌트 호출을 통한 렌더링 요소를 할당 */}
+  <TestReact />
+  <TestReact2 />
+  </>
+);
