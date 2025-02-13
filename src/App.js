ce40a219b2bce689d8b06a5320697c243dba0f26
believehan <90814456+believehan@users.noreply.github.com>
2025-02-13 22:25:53 +0900

    에제3번 시작

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import InputOutputClient from "./subModule/InputOutputClient";
-// 값 받아서 동기화 하고 키보드엔터 이벤트 구현
-function App() {
-    return (
-        <InputOutputClient />
-    );
+
+import NationContainer from './NationContainer';
+const imgList = ['./images/bg1.jpg', './images/bg2.jpg', './images/bg3.jpg','./images/bg4.jpg'];
+const selectIdx = [];
+function clickNation() {
+
 }
+function App() {
+  return (
+    <div className="main_container">
+      <img src={imgList[selectIdx]} alt="imgBox" />
+
+      <div className='nation_container' onClick={clickNation}>
+        <NationContainer
+          imgList={imgList}
+          selectIdx={selectIdx}
+        />
+      </div>
+    </div>
+  );
+}//함수는 다지웠음 
 
 export default App;
