daa0a2a790d2f1b923a405fffea6598364e4e193
believehan <90814456+believehan@users.noreply.github.com>
2025-02-21 09:40:48 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 export default function App() {
-
+   
    function a() {
-      return (
-         <div>
-            <p>취업하고 싶다</p>
-            <span>집가고 싶다</span>
-         </div>
+      return(
+      <div>
+         <p>취업하고 싶다</p>
+         <span>집가고 싶다</span>
+      </div>
       );
    }
 
-   return (
+   return(
       <>
          <h1>h1</h1>
          <div>{a()}</div>
