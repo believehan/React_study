c3b439a0a65b77277f93c0d2aeb2f6592cb6f88a
believehan <90814456+believehan@users.noreply.github.com>
2025-03-14 12:21:42 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react"; //안써도 됨
 import ReactDOM from 'react-dom/client';
 import App from "./App";
-// import { BrowserRouter } from 'react-router-dom'; //이거 추가하고
+import { BrowserRouter } from 'react-router-dom'; //이거 추가하고
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 
-  // <BrowserRouter> {/* App.js 감쌈 */}
+  <BrowserRouter> {/* App.js 감쌈 */}
     <App />
-  // </BrowserRouter>
+  </BrowserRouter>
 );
 //이곳에 조립하면 제약상황이 생김 그래서 이곳은 그냥 랜더하는 역할만 함
