3d36fc127ec7e250bd1a6ff2e795a3c49c21489a
believehan <90814456+believehan@users.noreply.github.com>
2025-07-18 09:44:12 +0900

    백엔드 연결 테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import axios from 'axios';
 
 export default function App() {
-   async function getServletData() {
-      const pubDelData = {
-         pubTrueId_: [54, 53],
-         pubFalseId_: [52, 51],
-         pudDelBtn: 'batchPubBtn',
-      };
+  async function getServletData() {
+    const pubDelData = {
+      pubTrueId_: [1, 2],
+      pubFalseId_: [3, 4],
+      pudDelBtn: '한글',
+    };
 
-      const servletData = await axios.post(`${process.env.REACT_APP_API_URL}view/admin/notice/list`, pubDelData);
+    const servletData = await axios.post(`${process.env.REACT_APP_API_URL}test`, pubDelData);
 
-      console.log(servletData.data);
-   }
+    console.log(servletData.data);
+  }
 
-   return (
-      <>
-         <h1>React 와 스프링 CORS 필터 설정 테스트</h1>
-         <button onClick={getServletData}>테스트</button>
-         <hr />
-      </>
-   );
+  return (
+    <>
+      <h1>React 와 스프링 부트 CORS 필터 설정 테스트</h1>
+      <button onClick={getServletData}>테스트</button>
+      <hr />
+    </>
+  );
 }
